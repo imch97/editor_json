7a3dd856c1c1dab24f8878ca12e6bdde30fc1d9a
Ivan_WEB <ivan.chuvilko@gmail.com>
2021-02-09 16:11:56 +0300

    git pages

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -4,15 +4,26 @@ import { useState, useEffect } from 'react'
 import classNames from 'classnames'
 
 const Field = ({ fieldKey, value }) => {
+	const [key, setKey] = useState(fieldKey)
+	const [valueField, setValueField] = useState(value)
+
+	const handleChange = (event) => {
+		const content = event.target.textContent
+		setValueField(content)
+		console.log(valueField)
+	}
+
 	return (
 		<div className={classNames({ item: true })}>
-			<div className={classNames({ item_key: true })}>{fieldKey}:</div>
+			<div className={classNames({ item_key: true })}>{key}:</div>
 			<div
 				className={classNames({ item_value: true })}
 				contentEditable="true"
 				suppressContentEditableWarning="true"
+				onClick={(e) => handleChange(e)}
+				value={valueField}
 			>
-				{value}
+				{valueField}
 			</div>
 		</div>
 	)
